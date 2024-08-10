1539a98f0328e29e4bdd9cbb1b6cca62e1459676
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-10 20:56:17 -0300

    first commit

---
+++ src/app/components/navbar/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/components/styles/pages.module.css';
+
+export default function NavBar() {
+    return (
+        <nav className={styles.navbar}>
+            <h1>List of Films</h1>
+        </nav>
+    );
+};
